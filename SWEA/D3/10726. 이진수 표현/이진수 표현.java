fb8787bf504a1cfbdc2597b2339348f0b8704d8e
olrlozl <108621930+olrlozl@users.noreply.github.com>
2023-08-30 11:29:15 +0900

    [D3] Title: 이진수 표현, Time: 156 ms, Memory: 32,680 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -9,21 +9,18 @@ public class Solution {
 		StringTokenizer st;
 
 		int T = Integer.parseInt(br.readLine());
-		
+
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
-			
-			// N자리수만큼 1로 채우기
-			int n = 0;
-			for (int i = 0; i < N; i++) n = n * 2 + 1;
-			
-			if ((M & n) == n) bw.write("#" + t + " ON\n");
+
+			// N자리수만큼 1로 채운 수 : (1 << N) - 1
+			if ((M & ((1 << N) - 1)) == ((1 << N) - 1)) bw.write("#" + t + " ON\n");
 			else bw.write("#" + t + " OFF\n");
+            
 		}
-		
-		bw.close();		
+		bw.close();
 	}
-	
+
 }
