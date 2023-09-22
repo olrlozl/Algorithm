65bccf0146908324bd7d17939027585e8f0446bf
olrlozl <108621930+olrlozl@users.noreply.github.com>
2023-09-22 09:07:26 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 885 ms, Memory: 146,032 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,30 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
+    public static class Edge implements Comparable<Edge> {
+        int v;
+        long cost;
 
-    public static void union (int[] parent, int a, int b) {
-        a = find(parent, a);
-        b = find(parent, b);
-        if (a < b) parent[b] = a;
-        else parent[a] = b;
-    }
-
-    public static int find(int[] parent, int x) {
-        if (parent[x] == x) return x;
-        else return parent[x] = find(parent, parent[x]);
-    }
-
-    public static long kruskal(long[][] graph, int[] parent) {
-        long cost = 0;
-
-        for (int i = 0; i < graph.length; i++) {
-            if (find(parent, (int)graph[i][0]) != find(parent, (int)graph[i][1])) {
-                cost += graph[i][2];
-                union(parent, (int)graph[i][0], (int)graph[i][1]);
-            }
+        public Edge (int v, long cost) {
+            this.v = v;
+            this.cost = cost;
         }
 
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost > 0 ? 1 : -1; // cost 기준 오름차순 정렬
+        }
+    }
+
+    public static long prim (ArrayList<Edge>[] graph, int start, int N) {
+        long cost = 0;
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        boolean[] visit = new boolean[N];
+
+        pq.add(new Edge(start, 0));
+
+        while (!pq.isEmpty()) { // 우선순위 큐가 빌 때 까지
+            Edge now = pq.poll(); // 우선순위 큐에서 정점 하나를 꺼내고
+
+            if (visit[now.v]) continue; // 이미 방문한 정점이라면 건너뛰기
+
+            // 아직 방문하지 않은 정점이라면
+            visit[now.v] = true; // 방문처리
+            cost += now.cost; // 가중치 합하기
+
+            for (Edge next : graph[now.v]) { // 이웃한 정점 중에서
+                if (!visit[next.v]) { // 아직 방문하지 않은 정점이 있다면
+                    pq.add(new Edge(next.v, next.cost)); // 우선순위 큐에 넣기
+                }
+            }
+        }
         return cost;
     }
 
@@ -34,38 +49,30 @@ public class Solution {
         StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
-
         for (int t = 1; t <= T; t++) {
-            int n = Integer.parseInt(br.readLine()); // 섬의 개수
+            int N = Integer.parseInt(br.readLine()); // 섬의 개수
 
-            // x좌표
-            long[] x = new long[n];
+            int[] x = new int[N]; // x 좌표
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) x[i] = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < N; i++) x[i] = Integer.parseInt(st.nextToken());
 
-            // y좌표
-            long[] y = new long[n];
+            int[] y = new int[N]; // y 좌표
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) y[i] = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < N; i++) y[i] = Integer.parseInt(st.nextToken());
 
             double E = Double.parseDouble(br.readLine()); // 환경 부담 세율
 
-            long[][] graph = new long[n * (n-1) / 2][3];
+            ArrayList<Edge>[] graph = new ArrayList[N];
+            for (int i = 0; i < N; i++) graph[i] = new ArrayList<>();
 
-            for (int i = 0, idx = 0; i < n-1; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    graph[idx][0] = i; // x좌표
-                    graph[idx][1] = j; // y좌표
-                    graph[idx++][2] = (long) (Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2)); // cost
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i != j) {
+                        graph[i].add(new Edge(j, (long) (Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2))));
+                    }
                 }
             }
-
-            Arrays.sort(graph, (o1, o2) -> o1[2] - o2[2] >= 0 ? 1 : -1); // cost 기준 오름차순 정렬
-
-            int[] parent = new int[n];
-            for (int i = 0; i < n; i++) parent[i] = i;
-
-            bw.write("#" + t + " " + Math.round(E * kruskal(graph, parent)) + "\n");
+            bw.write("#" + t + " " + Math.round(E * prim(graph, 0, N)) + "\n");
         }
         bw.close();
     }
