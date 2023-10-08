6eb72e2fdf52aef338ebb02709677ecfbc466897
olrlozl <108621930+olrlozl@users.noreply.github.com>
2023-10-08 23:13:45 +0900

    [unrated] Title: 광물 캐기, Time: 8.06 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
@@ -20,11 +20,9 @@ class Solution {
     
     public static void Permutation(String[] minerals, int[] src, int[] tgt, boolean[] select, int tgtIdx) {
         if (tgtIdx == tgt.length) {
-//            System.out.println(Arrays.toString(tgt));
             Mining(minerals, tgt);
             return;
         }
-        // 현재 tgtIdx에 같은 종류의 곡괭이가 나오지 않도록 하기 위해,  방문여부 배열을 Permutation함수가 호출 될 때마다 생성
         boolean[] visit = new boolean[3];
         for (int i = 0; i < src.length; i++) {
             if (!select[i] && ! visit[src[i]]) {
@@ -38,18 +36,14 @@ class Solution {
    }
     
     public int solution(int[] picks, String[] minerals) {
-        // 가지고 있는 모든 곡괭이를 담은 배열
         int[] src = new int[picks[0] + picks[1] + picks[2]];
         for (int i = 0, idx = 0; i < 3; i++) {
             for (int j = 0; j < picks[i]; j++) {
                 src[idx++] = i;
             }
         }
-        // 사용할 곡괭이를 담을 배열
         int[] tgt = new int[Math.min(minerals.length / 5 + 1, src.length)];
-        // 곡괭이 선택 여부를 담을 배열
         boolean[] select = new boolean[src.length];
-        // 순열 함수 호출
         Permutation(minerals, src, tgt, select, 0);
 
         return minSum;
