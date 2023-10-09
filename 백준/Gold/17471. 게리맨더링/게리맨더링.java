055ff8e7072e0173ed12a7375996957fcdb55d54
olrlozl <108621930+olrlozl@users.noreply.github.com>
2023-10-09 18:22:40 +0900

    [Gold IV] Title: 게리맨더링, Time: 92 ms, Memory: 13552 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -2,86 +2,85 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int n;
+    public static int[] arr;
     public static ArrayList<Integer>[] graph;
     public static int min = Integer.MAX_VALUE;
 
-    public static void bfs (boolean[] tgt, boolean[] visit, int n, int i) {
+    public static void bfs(boolean[] tgt, boolean[] visit, int start) {
         Queue<Integer> queue = new LinkedList<>();
-        boolean state = tgt[i];
 
-        queue.add(i);
-        visit[i] = true;
+        queue.add(start);
+        visit[start] = true;
 
         while (!queue.isEmpty()) {
             int now = queue.poll();
             for (int next : graph[now]) {
-                if (!visit[next] && tgt[next] == state) {
+                if (!visit[next] && tgt[next] == tgt[start]) {
                     queue.add(next);
                     visit[next] = true;
                 }
             }
         }
     }
-    public static void combination (boolean[] tgt, int[] vote, int n, int r, int idx, int cnt) {
+
+    public static void Combination(boolean[] tgt, int r, int idx, int cnt) {
         if (cnt == r) {
             boolean[] visit = new boolean[n + 1];
-            int count = 0; // bfs 호출 횟수
-
+            int count = 0;
             for (int i = 1; i <= n; i++) {
                 if (!visit[i]) {
-                    bfs(tgt, visit, n, i);
+                    bfs(tgt, visit, i);
                     count++;
                 }
-
             }
-            if (count == 2) { // 두 그룹으로 나눠지고 각각의 그룹이 서로 연결되어있다면
-                int A = 0; // A구역 인원 수
-                int B = 0; // B구역 인원 수
+            if (count == 2) {
+                int total = 0;
                 for (int i = 1; i <= n; i++) {
-                    if (tgt[i]) A += vote[i];
-                    else B += vote[i];
+                    if (tgt[i]) total += arr[i];
+                    else total -= arr[i];
                 }
-                min = Math.min(min, Math.abs(A - B));
+                min = Math.min(min, Math.abs(total));
             }
-
             return;
         }
 
-        if (idx == n + 1) return;
+        if (idx == tgt.length) return;
 
         tgt[idx] = true;
-        combination(tgt, vote, n, r, idx + 1, cnt + 1);
+        Combination(tgt, r, idx + 1, cnt + 1);
         tgt[idx] = false;
-        combination(tgt, vote, n, r, idx + 1, cnt);
+        Combination(tgt, r, idx + 1, cnt);
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine()); // 구역 개수
-        int[] vote = new int[n + 1]; // 인구 수
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) vote[i] = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine()); // 구역의 개수
 
-        graph = new ArrayList[n + 1];
+        arr = new int[n + 1]; // 각 구역의 인구 수
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1]; // 각 구역과 인접한 구역의 정보
         for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < m; j++) {
+            int k = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < k; j++) {
                 graph[i].add(Integer.parseInt(st.nextToken()));
             }
         }
 
+        // n개 구역중 r개 구역을 뽑자
         for (int r = 1; r <= n / 2; r++) {
-            boolean[] tgt = new boolean[n + 1];
-            combination(tgt, vote, n, r, 1, 0);
+            Combination(new boolean[n + 1], r, 1, 0);
         }
 
-        bw.write((min != Integer.MAX_VALUE ? min : -1) + "");
+        bw.write((min == Integer.MAX_VALUE ? -1 : min) + "");
         bw.close();
     }
 }
