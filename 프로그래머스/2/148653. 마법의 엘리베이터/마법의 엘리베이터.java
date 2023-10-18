2aa48bd7f52bfdea18af55b4ad259899f44aef5e
olrlozl <108621930+olrlozl@users.noreply.github.com>
2023-10-18 11:26:21 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.17 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -3,15 +3,14 @@ class Solution {
 
     public void elevator(int n, int m, int cnt, int v) {
         if (n == 0) {
-                    min = Math.min(min, cnt);
-                    return;
-                }
+            min = Math.min(min, cnt);
+            return;
+        }
 
-                if (m <= v) elevator((int)(n + Math.pow(10, m + 1) - n % (Math.pow(10, m + 1))), m + 1, cnt + (int)((Math.pow(10, m + 1) - n % (Math.pow(10, m + 1))) / Math.pow(10, m)), v);
-                elevator((int)(n - n % (Math.pow(10, m + 1))), m + 1, cnt + (int)(n % (Math.pow(10, m + 1)) / Math.pow(10, m)), v);
-
-
+        if (m <= v) elevator((int)(n + Math.pow(10, m + 1) - n % (Math.pow(10, m + 1))), m + 1, cnt + (int)((Math.pow(10, m + 1) - n % (Math.pow(10, m + 1))) / Math.pow(10, m)), v);
+        elevator((int)(n - n % (Math.pow(10, m + 1))), m + 1, cnt + (int)(n % (Math.pow(10, m + 1)) / Math.pow(10, m)), v);
     }
+    
     public int solution(int storey) {
         elevator(storey, 0, 0, (int)(Math.log10(storey) + 1));
         return min;
