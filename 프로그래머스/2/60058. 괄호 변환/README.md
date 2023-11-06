c5faa6af1dc6c6ed2c887e1fc9db3b62ed0b28a3
olrlozl <108621930+olrlozl@users.noreply.github.com>
2023-11-06 14:54:15 +0900

    [level 2] Title: 괄호 변환, Time: 16.98 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/README.md
+++ 프로그래머스/2/60058. 괄호 변환/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 변환 - 60058 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 21.11 ms
+메모리: 81 MB, 시간: 16.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 14:49:51
+2023년 11월 1일 14:56:14
 
 ### 문제 설명
 
--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -4,9 +4,7 @@ class Solution {
         public Boolean isRight(String str) {
         Stack<Character> stack = new Stack<>();
         for (char c : str.toCharArray()) {
-            if (c == '(') {
-                stack.add(c);
-            }
+            if (c == '(') stack.add(c);
             else {
                 if (stack.isEmpty()) return false;
                 stack.pop();
@@ -37,12 +35,9 @@ class Solution {
     }
     public String solution(String p) {
         if (p.isEmpty()) return p; // 1. 빈 문자열인 경우, 빈 문자열을 반환
-
         String[] uv = seperate(p); // 2. p를 두 "균형잡힌 괄호 문자열" u, v로 분리
 
-        // 3. u가 "올바른 괄호 문자열" 이라면
-        if (isRight(uv[0])) return uv[0] + solution(uv[1]);
-        // 4. u가 "올바른 괄호 문자열"이 아니라면
-        else return "(" + solution(uv[1]) + ")" + reverse(uv[0]);
+        if (isRight(uv[0])) return uv[0] + solution(uv[1]); // 3. u가 "올바른 괄호 문자열" 이라면
+        else return "(" + solution(uv[1]) + ")" + reverse(uv[0]); // 4. u가 "올바른 괄호 문자열"이 아니라면
     }
 }
