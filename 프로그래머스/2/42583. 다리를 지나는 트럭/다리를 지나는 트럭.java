426aedfd1f8bd9c5dc6404a3755257c1dbb1e2c8
olrlozl <108621930+olrlozl@users.noreply.github.com>
2023-11-08 18:32:46 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.20 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -10,26 +10,23 @@ class Solution {
         for (int i = 0; i < bridge_length; i++) cross.add(0);
         int cross_weight = 0; // 다리 건너는 트럭 무게 합
 
-        // 모든 트럭이 다리를 지날 때 까지
-        while (!wait.isEmpty() || cross_weight > 0) {
-            // 다리 건너는 트럭이 있다면
-            if (cross_weight > 0) {
-                // 맨 마지막 트럭 다리 탈출
-                if (cross.get(bridge_length - 1) > 0)
-                    cross_weight -= cross.get(bridge_length - 1);
-                // 다리 건너는 트럭 한 칸씩 전진
-                for (int i = bridge_length - 2; i >= 0; i--) {
-                    cross.set(i + 1, cross.get(i));
-                }
-                cross.set(0, 0);
-            }
+        // 대기 트럭이 없을 때까지
+        while (!wait.isEmpty()) {
+            // 다리 끝 지점에 있던 트럭 탈출
+            cross_weight -= cross.get(bridge_length - 1);
+            cross.remove(bridge_length - 1);
+
             // 대기 트럭이 출발 가능 하다면
-            if (!wait.isEmpty() && cross_weight + wait.peek() <= weight) {
+            if (cross_weight + wait.peek() <= weight) {
                 cross_weight += wait.peek();
-                cross.set(0, wait.poll());
+                cross.add(0, wait.poll());
             }
+            // 대기 트럭이 출발 불가능 하다면
+            else cross.add(0, 0);
+
             time++;
         }
-        return time;
+        // 현재, 마지막 대기 트럭이 다리 시작 지점에 있는 상황.
+        return time + bridge_length;
     }
 }
