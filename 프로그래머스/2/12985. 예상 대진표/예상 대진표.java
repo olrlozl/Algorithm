657452062a097ad0e9083d3095c497e4de9d688a
olrlozl <108621930+olrlozl@users.noreply.github.com>
2023-12-31 16:48:04 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int n, int a, int b) {
-        int round = 1;
+        int round = 0;
 
-        while ((a + 1) / 2 != (b + 1) / 2) {
+        while (a != b) {
             a = (a + 1) / 2;
             b = (b + 1) / 2;
-            System.out.println(a + " " + b);
             round++;
         }
 
