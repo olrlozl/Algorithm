28ddc97d802983494b945d1b40253a1a77cc35a2
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-01-07 15:30:11 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int cnt = 0;
+        
+        HashMap<String, Integer> wantMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) wantMap.put(want[i], number[i]);
+
+        for (int i = 0; i <= discount.length - 10; i++) {
+            HashMap<String, Integer> discountMap = new HashMap<>();
+            for (int j = i; j < i + 10; j++) discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
+            if (wantMap.equals(discountMap)) cnt++;
+        }
+        return cnt;
+    }
+}
