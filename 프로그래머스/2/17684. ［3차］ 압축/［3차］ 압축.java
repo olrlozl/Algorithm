067ca4e59e79be320a7a220183032b7d9bddfdd9
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-03-04 23:31:23 +0900

    [level 2] Title: [3차] 압축, Time: 1.93 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String msg) {
+        
+        List<String> dic = new ArrayList<>();
+        dic.add("");
+        char ch = 'A';
+        for (int i = 0; i < 26; i++) dic.add(String.valueOf(ch++)); 
+        
+        List<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(msg);
+        int start = 0;
+        int end = 1;
+
+        while (end <= msg.length()) {
+            if (dic.contains(sb.substring(start, end))) {
+                end++;
+            } else {
+                list.add(dic.indexOf(sb.substring(start, end - 1))); // 출력
+                dic.add(sb.substring(start,end)); // 사전 추가
+                start = end - 1;
+            }
+        }
+        list.add(dic.indexOf(sb.substring(start, end - 1))); // 출력
+        
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) result[i] = list.get(i);
+        
+        return result;
+    }
+}
