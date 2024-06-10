855900c13ad7ad641a97dba3a8d681c03beb9cc7
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-06-10 22:13:15 +0900

    [level 3] Title: 네트워크, Time: 0.62 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,41 +1,47 @@
-// System.out.println()
 import java.util.*;
 
 class Solution {
-    
-    public void dfs(ArrayList<Integer>[] graph, boolean[] visit, int now) {
-        visit[now] = true;
+    public void bfs(ArrayList<Integer>[] graph, boolean[] visit, int x) {
+        Queue<Integer> queue = new LinkedList<>();
         
-        for (int next : graph[now]) {
-            if (!visit[next]) {
-                visit[next] = true;
-                dfs(graph, visit, next);
-            }    
+        queue.add(x);
+        visit[x] = true;
+        
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int next : graph[cur]) {
+                if (!visit[next]) {
+                    queue.add(next);
+                    visit[next] = true;
+                }
+            }
         }
     }
     
     public int solution(int n, int[][] computers) {
-        ArrayList<Integer>[] graph = new ArrayList[n];
-        for (int i = 0; i < n ; i++) graph[i] = new ArrayList<>();
+        int cnt = 0;
+        
+        ArrayList<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i != j && computers[i][j] == 1) {
-                    graph[i].add(j);
+                    graph[i + 1].add(j + 1);
                 }
             }
         }
         
-        boolean[] visit = new boolean[n];
-        int count = 0;
-        
-        for (int i = 0; i < n; i++) {
+        boolean[] visit = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
             if (!visit[i]) {
-                dfs(graph, visit, i);
-                count++;
+                bfs(graph, visit, i);
+                cnt++;
             }
         }
-
-        return count;
+        
+        return cnt;
     }
 }
