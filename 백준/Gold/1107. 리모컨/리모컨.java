80755ec5d56e3e4f698923bed9ca7a0626ee2f13
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-07-05 18:04:51 +0900

    [Gold V] Title: 리모컨, Time: 100 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -2,14 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int N;
+    public static boolean[] broken;
+    public static int answer;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine()); // 타겟 채널
+        N = Integer.parseInt(br.readLine()); // 타겟 채널
         int M = Integer.parseInt(br.readLine()); // 고장난 버튼 개수
-        boolean[] broken = new boolean[10]; // 정상(F) 고장(T)
+        broken = new boolean[10]; // 정상(F) 고장(T)
         if (M > 0) {
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < M; i++) {
@@ -17,24 +21,23 @@ public class Main {
             }
         }
 
-        int cnt = Math.abs(100 - N); // 현재 채널 100에서 타겟 채널 N까지 +- 버튼으로만 이동하는 경우
+        answer = Math.abs(N - 100);
+        fn(0, 0);
+        bw.write(answer + "");
+        bw.close();
+    }
 
-        // 현재 채널 100에서 숫자 버튼으로 i로 이동 후에, 타겟 채널 N까지 +- 버튼으로 이동하는 경우
-        for (int i = 0; i <= 999999; i++) {
-            String num = Integer.toString(i);
-            boolean isbroken = false;
-            for (char c : num.toCharArray()) {
-                if (broken[c - '0']) {
-                    isbroken = true;
-                    break;
-                }
-            }
-            if (!isbroken) { // 숫자 버튼으로 i로 이동할 수 있는 경우에만
-                cnt = Math.min(cnt, num.length() + Math.abs(N - i));
-            }
+    public static void fn(int depth, int cur) {
+        if (depth > 0) {
+            answer = Math.min(answer, depth + Math.abs(N - cur));
         }
 
-        bw.write(cnt + "");
-        bw.close();
+        if (depth == 6) return;
+
+        for (int i = 0; i <= 9; i++) {
+            if (!broken[i]) {
+                fn(depth + 1, cur * 10 + i);
+            }
+        }
     }
 }
