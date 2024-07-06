fd4f11cd1b22741391b7bff05409096d567df8f6
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-07-06 22:03:13 +0900

    [level 3] Title: 야근 지수, Time: 110.88 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int work : works) {
+            pq.add(work);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (pq.isEmpty()) break;
+            int work = pq.poll();
+            if (work > 1) {
+                pq.add(work - 1);
+            } 
+        }
+        
+        long answer = 0;
+        int size = pq.size();
+        for (int i = 0; i < size; i++) {
+            int work = pq.poll();
+            answer += Math.pow(work, 2);
+        }
+        
+        return answer;
+    }
+}
