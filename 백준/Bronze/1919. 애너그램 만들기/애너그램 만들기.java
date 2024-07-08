c843f77826e8dfc6bb82ea683da1919146cf8884
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-07-08 20:14:16 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 68 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,33 +1,17 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String a = br.readLine();
-        String b = br.readLine();
-
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-        
-        for (char c : a.toCharArray()) {
-            map1.put(c, map1.getOrDefault(c, 0) + 1);
-        }
-        for (char c : b.toCharArray()) {
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
-        }
+        int[] cnt = new int[26];
+        for (char c : br.readLine().toCharArray()) cnt[c - 'a']++;
+        for (char c : br.readLine().toCharArray()) cnt[c - 'a']--;
 
         int result = 0;
-        for (char key : map1.keySet()) {
-            result += Math.abs(map1.get(key) - map2.getOrDefault(key, 0));
-        }
-        for (char key : map2.keySet()) {
-            if (map1.getOrDefault(key, 0) > 0) continue;
-            result += map2.get(key);
-        }
-        
+        for (int i = 0; i < 26; i++) result += Math.abs(cnt[i]);
+
         bw.write(result + "");
         bw.close();
     }
