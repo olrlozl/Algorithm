b0e7a4faa8468d83d66cb0114df9d07289ce5270
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-07-15 22:42:14 +0900

    [Gold V] Title: 토마토, Time: 572 ms, Memory: 128980 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,48 +1,15 @@
-import java.awt.*;
 import java.io.*;
 import java.util.*;
+import java.awt.*;
 
 public class Main {
-
-    public static int N; // 가로
-    public static int M; // 세로
-
-    public static int bfs (int[][] box) {
-        int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상 하 좌 우
-        Queue<Point> queue = new LinkedList<>();
-
-        // 익은 토마토를 모두 큐에 넣기
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < M; c++) {
-                if (box[r][c] == 1) {
-                    queue.add(new Point(r,c));
-                }
-            }
-        }
-
-        while (!queue.isEmpty()) { // 큐가 빌 때 까지
-            Point tomato = queue.poll(); // 큐에서 토마토 하나를 꺼내고
-            // 상하좌우를 확인해보면서
-            for (int d = 0; d < 4; d++) {
-                int nr = tomato.x + delta[d][0];
-                int nc = tomato.y + delta[d][1];
-                // 이웃 토마토 중에 익지 않은 토마토가 있다면
-                if (0 <= nr && nr < N && 0 <= nc && nc < M && box[nr][nc] == 0) {
-                    queue.add(new Point(nr, nc)); // 큐에 넣고
-                    box[nr][nc] = box[tomato.x][tomato.y] + 1; // 방문 처리
-                }
-            } // for
-        } // while
-
-        int max = 0;
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < M; c++) {
-                if (box[r][c] == 0) return -1; // 익지 않은 토마토가 있다면 -1
-                if (box[r][c] > max) max = box[r][c];
-            }
-        }
-        return max - 1; // 토마토가 모두 익을 때까지 최소 날짜
-    }
+    public static int M; // 가로
+    public static int N; // 세로
+    public static int[][] arr; // 상자 속 토마토 정보
+    public static ArrayList<Point> ripe; // 익은 토마토
+    public static int unripe; // 익지 않은 토마토 개수
+    public static int day = 0; // 모든 토마토가 익기 위한 최소 일수
+    public static int[][] delta = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -50,19 +17,49 @@ public class Main {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());// 가로 칸의 수
-        N = Integer.parseInt(st.nextToken());// 세로 칸의 수
+        M = Integer.parseInt(st.nextToken()); // 가로
+        N = Integer.parseInt(st.nextToken()); // 세로
 
-        int[][] box = new int[N][M];
-
-        for (int r = 0; r < N; r++) {
+        arr = new int[N][M];
+        ripe = new ArrayList<>(); // 익은 토마토
+        unripe = 0; // 익지 않은 토마토 개수
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < M; c++) {
-                box[r][c] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
+                    ripe.add(new Point(i, j));
+                } else if (arr[i][j] == 0) {
+                    unripe++;
+                }
             }
         }
 
-        bw.write(bfs(box) + "");
+        if (unripe > 0) bfs();
+        bw.write((unripe == 0 ? day : -1) + "");
         bw.close();
+    }
+
+    public static void bfs() {
+        Queue<Point> queue = new LinkedList<>(ripe);
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            boolean flag = false;
+            for (int i = 0; i < len; i++) {
+                Point now = queue.poll();
+                for (int d = 0; d < 4; d++) {
+                    int nr = now.x + delta[d][0];
+                    int nc = now.y + delta[d][1];
+                    if (0 <= nr && nr < N && 0 <= nc && nc < M && arr[nr][nc] == 0) {
+                        queue.add(new Point(nr, nc));
+                        arr[nr][nc] = 1;
+                        unripe--;
+                        flag = true;
+                    }
+                }
+            }
+            if (flag) day++;
+        }
     }
 }
