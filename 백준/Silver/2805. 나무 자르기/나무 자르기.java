c3402b2727318a1179ebc5370c34e9fc1f3e1b0c
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-07-16 20:15:26 +0900

    [Silver II] Title: 나무 자르기, Time: 472 ms, Memory: 168724 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 나무 수
+        int M = Integer.parseInt(st.nextToken()); // 필요한 최소 나무 길이
+
+        int[] arr = new int[N];
+        int min = 0;
+        int max = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+
+        while (min < max) {
+            int mid = (min + max) / 2;
+
+            long sum = 0;
+            for (int a : arr) if (a - mid > 0) sum += a - mid;
+
+            if (sum < M) max = mid;
+            else min = mid + 1;
+        }
+
+        bw.write((min - 1) + "");
+        bw.close();
+    }
+}
