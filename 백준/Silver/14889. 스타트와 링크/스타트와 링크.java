9c91b6c5c3d5e6265828fddca3d61a535ec2f79d
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-07-30 20:57:57 +0900

    [Silver I] Title: 스타트와 링크, Time: 320 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -3,52 +3,28 @@ import java.util.*;
 
 public class Main {
     public static int[][] arr;
-    public static int score;
-    public static int factorial;
     public static int min = Integer.MAX_VALUE;
 
-
-    public static int Permutation(int[] team, boolean[] visit, int[] tgt, int idx) {
-        if (idx == 2) {
-            score += arr[tgt[0]][tgt[1]];
-            return score;
-        }
-        for (int i = 0; i < team.length; i++) {
-            if (!visit[i]) {
-                tgt[idx] = team[i];
-                visit[i] = true;
-                Permutation(team, visit, tgt, idx + 1);
-                visit[i] = false;
-            }
-        }
-        return score;
-    }
-
-    public static void Combination(boolean[] src, int idx, int cnt) {
-        if (cnt == src.length / 2) {
-            if (factorial-- == 0) return;
-            int[] A = new int[src.length / 2];
-            int[] B = new int[src.length / 2];
-            for (int i = 0, a = 0, b = 0; i < src.length; i++) {
-                if (src[i]) A[a++] = i;
-                else B[b++] = i;
+    public static void Combination(boolean[] team, int idx, int cnt) {
+        if (cnt == team.length / 2) {
+            int score = 0;
+            for (int i = 0; i < team.length - 1; i++) {
+                for (int j = i + 1; j < team.length; j++) {
+                    if (team[i] && team[j]) score += arr[i][j] + arr[j][i];
+                    else if (!team[i] && !team[j]) score -= arr[i][j] + arr[j][i];
+                }
             }
 
-            score = 0;
-            int Ascore = Permutation(A, new boolean[src.length / 2], new int[2], 0);
-            score = 0;
-            int Bscore = Permutation(B, new boolean[src.length / 2], new int[2], 0);
-
-            min = Math.min(min, Math.abs(Ascore - Bscore));
+            min = Math.min(min, Math.abs(score));
             return;
         }
 
-        if (idx == src.length) return;
+        if (idx == team.length) return;
 
-        src[idx] = true;
-        Combination(src, idx + 1, cnt + 1);
-        src[idx] = false;
-        Combination(src, idx + 1, cnt);
+        team[idx] = true;
+        Combination(team, idx + 1, cnt + 1);
+        team[idx] = false;
+        Combination(team, idx + 1, cnt);
     }
 
     public static void main(String[] args) throws IOException {
@@ -65,8 +41,6 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        factorial = 1;
-        for (int i = 1; i <= n; i++) factorial *= i;
 
         Combination(new boolean[n], 0, 0);
 
