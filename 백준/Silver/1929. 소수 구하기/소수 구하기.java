3a214594cd6d51fb1414c6d01900325ba42fcd38
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-08-04 18:31:41 +0900

    [Silver III] Title: 소수 구하기, Time: 176 ms, Memory: 22876 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -5,21 +5,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
-        for (int i = M; i <= N; i++) {
-            boolean isPrime = true;
-            for (int j = 2; j <= (int) Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
+        // 에라토스테네스의 체
+        boolean[] arr = new boolean[N + 1]; // 소수(F) 소수아님(T)
+        arr[0] = true;
+        arr[1] = true;
+        
+        for (int i = 2; i <= (int) Math.sqrt(N); i++) {
+            if (arr[i]) continue;
+            for (int j = i * i; j <= N; j += i) { // i * i 부터 확인하면 됨
+                arr[j] = true; // i의 배수들 소수아님
             }
-            if (i > 1 && isPrime) bw.write(i + "\n");
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (!arr[i]) bw.write(i + "\n");
         }
         bw.close();
     }
