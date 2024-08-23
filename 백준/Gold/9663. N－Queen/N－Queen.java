5dcfc33e6b4c5f0e99700c5e4811d088edcf0707
olrlozl <108621930+olrlozl@users.noreply.github.com>
2024-08-23 21:50:18 +0900

    [Gold IV] Title: N-Queen, Time: 4776 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Main {
     public static int N;
     public static int[] arr;
-    public static int[][] delta = {{-1, -1}, {-1, 0}, {-1, 1}}; // 좌상, 상, 우상
     public static int cnt;
 
     public static void main(String[] args) throws IOException {
@@ -15,6 +14,7 @@ public class Main {
         bw.write(cnt + "");
         bw.close();
     }
+
     // r행 에서 퀸을 놓을 열 정하기
     public static void simulate(int r) {
         if (r == N) {
@@ -30,13 +30,9 @@ public class Main {
     }
     // r행 c열에 퀸을 놓을 수 있는지 확인
     public static boolean isPossible(int r, int c) {
-        for (int i = 0; i < r; i++) {
-            for (int d = 0; d < 3; d++) {
-                int pr = r + delta[d][0] * (r - i);
-                int pc = c + delta[d][1] * (r - i);
-                if (0 <= pr && pr < N && 0 <= pc && pc < N && arr[pr] == pc) {
-                    return false;
-                }
+        for (int pr = 0; pr < r; pr++) {
+            if (c == arr[pr] || Math.abs(c - arr[pr]) == r - pr) {
+                return false;
             }
         }
         return true;
