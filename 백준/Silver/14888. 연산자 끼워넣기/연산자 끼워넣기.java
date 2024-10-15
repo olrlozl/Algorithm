066570fbcc24b7c389abf223534e9e1e8c32bea0
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2024-10-15 20:40:50 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 148 ms, Memory: 18684 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -2,64 +2,69 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int n;
-    public static int[] arr;
+    public static int N;
+    public static int[] nums;
+    public static int[] operators;
     public static int max = Integer.MIN_VALUE;
     public static int min = Integer.MAX_VALUE;
-
-    public static void Permutation(int[] src, int[] tgt, boolean[] select, int tgtIdx) {
-        if (tgtIdx == tgt.length) {
-            int cal = arr[0];
-            for (int i = 0; i < n - 1; i++) {
-                if (tgt[i] == 0) cal += arr[i + 1];
-                else if (tgt[i] == 1) cal -= arr[i + 1];
-                else if (tgt[i] == 2) cal *= arr[i + 1];
-                else if (tgt[i] == 3) cal /= arr[i + 1];
-            }
-            max = Math.max(max, cal);
-            min = Math.min(min, cal);
-            return;
-        }
-
-        boolean[] visit = new boolean[4];
-        for (int i = 0; i < n - 1; i++) {
-            if (!select[i] && !visit[src[i]]) {
-                tgt[tgtIdx] = src[i];
-                visit[src[i]] = true;
-                select[i] = true;
-                Permutation(src, tgt, select, tgtIdx + 1);
-                select[i] = false;
-            }
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        operators = new int[N - 1];
 
-        arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
 
-        // + - * /
-        int[] src = new int[n - 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 0, idx = 0; i < 4; i++) {
-            int c = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < c; j++) {
-                src[idx++] = i;
+            int x = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < x; j++) {
+                operators[idx++] = i;
             }
         }
 
-        int[] tgt = new int[n - 1];
-        boolean[] select = new boolean[n - 1];
-
-        Permutation(src, tgt, select, 0);
+        int[] tgt = new int[N - 1];
+        boolean[] visit = new boolean[N - 1];
+        duplicate_permutation(tgt, visit, 0);
 
         bw.write(max + "\n" + min);
         bw.close();
+    }
+
+    public static void duplicate_permutation(int[] tgt, boolean[] visit, int tgtIdx) {
+        if (tgtIdx == N - 1) {
+            circulate(tgt);
+            return;
+        }
+
+        boolean[] select = new boolean[4];
+        for (int i = 0; i < N - 1; i++) {
+            if (!visit[i] && !select[operators[i]]) {
+                tgt[tgtIdx] = operators[i];
+                select[operators[i]] = true;
+                visit[i] = true;
+                duplicate_permutation(tgt, visit, tgtIdx + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+    public static void circulate(int[] tgt) {
+        int result = nums[0];
+        for (int i = 0; i < N - 1; i++) {
+            if (tgt[i] == 0) result += nums[i + 1];
+            else if (tgt[i] == 1) result -= nums[i + 1];
+            else if (tgt[i] == 2) result *= nums[i + 1];
+            else if (tgt[i] == 3) result /= nums[i + 1];
+        }
+        min = Math.min(min, result);
+        max = Math.max(max, result);
     }
 }
