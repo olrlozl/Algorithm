082f3abf557023da877b464659c50cd7bb640474
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2024-10-16 15:31:31 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 472 ms, Memory: 39168 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -4,46 +4,45 @@ import java.util.*;
 public class Main {
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void find (int[] parent, int x) throws IOException {
-        if (x != parent[x]) find(parent, parent[x]);
-        bw.write(x + " ");
-    }
-    
-    public static void bfs (int n, int k) throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         Queue<Integer> queue = new LinkedList<>();
         int[] visit = new int[100001];
         int[] parent = new int[100001];
-        for (int i = 1; i <= 100000; i++) parent[i] = i;
+        for (int i = 0; i < 100001; i++) parent[i] = i;
 
-        queue.add(n);
+        queue.add(N);
 
-        loop : while (!queue.isEmpty()) {
-            int now = queue.poll();
-            if (now == k) break;
-            int[] move = {now - 1, now + 1, now * 2};
-            for (int next : move) {
-                if (0 <= next && next <= 100000 && visit[next] == 0 && next != n) {
-                    queue.add(next);
-                    visit[next] = visit[now] + 1;
-                    parent[next] = now;
-                    if (next == k) break loop;
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+
+            for (int i = 0; i < len; i++) {
+                int now = queue.poll();
+                if (now == K) break;
+
+                int[] move = {now - 1, now + 1, now * 2};
+                for (int next : move) {
+                    if (0 <= next && next <= 100000 && visit[next] == 0 && next != N) {
+                        queue.add(next);
+                        visit[next] = visit[now] + 1;
+                        parent[next] = now;
+                    }
                 }
             }
         }
-        bw.write(visit[k] + "\n");
-        find(parent, k);
+
+        bw.write(visit[K] + "\n");
+        find(parent, K);
         bw.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        bfs(n, k);
+    public static void find(int[] parent, int x) throws IOException {
+        if (x != parent[x]) find(parent, parent[x]);
+        bw.write(x + " ");
     }
 }
