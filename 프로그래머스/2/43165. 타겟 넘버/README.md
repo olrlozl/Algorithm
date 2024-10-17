70b940bb7a83f7307b8979b2fd895d02bc202917
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2024-10-17 20:42:43 +0900

    [level 2] Title: 타겟 넘버, Time: 12.98 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 0.40 ms
+메모리: 80.8 MB, 시간: 12.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 22:25:48
+2024년 10월 17일 20:42:41
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,19 @@
 class Solution {
-    public static int cnt = 0;
-    
-    public void recursive(int[] numbers, int target, int sum, int idx) {
-        if (idx == numbers.length) {
-            if (sum == target) cnt++;
-            return;
-        }
-        recursive(numbers, target, sum + numbers[idx], idx + 1);
-        recursive(numbers, target, sum - numbers[idx], idx + 1);
-    }
+    public int cnt = 0;
     
     public int solution(int[] numbers, int target) {
         recursive(numbers, target, 0, 0);
         return cnt;
+    }
+    
+    public void recursive(int[] numbers, int target, int sum, int idx) {
+        if (numbers.length == idx) {
+            if (sum == target) {
+                cnt++;
+            }
+            return;
+        }
+        recursive(numbers, target, sum + numbers[idx], idx + 1);
+        recursive(numbers, target, sum - numbers[idx], idx + 1);
     }
 }
