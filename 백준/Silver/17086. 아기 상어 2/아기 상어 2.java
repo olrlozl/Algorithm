cb1cc16ee8a026d730e2dfcfe9701390875cc9e3
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2024-10-17 14:44:03 +0900

    [Silver II] Title: 아기 상어 2, Time: 116 ms, Memory: 14764 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -7,6 +7,8 @@ public class Main {
     public static int M;
     public static int[][] arr;
     public static int[][] delta = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, 1}, {-1, -1}, {1, 1}, {1, -1}};
+    public static ArrayList<Point> sharks;
+    public static int maxDist = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,51 +20,42 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         arr = new int[N][M];
+        sharks = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
+                    sharks.add(new Point(i, j));
+                }
             }
         }
 
-        int maxDist = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                maxDist = Math.max(maxDist, bfs(i, j));
-            }
-        }
+        bfs();
 
         bw.write(maxDist + "");
         bw.close();
     }
 
-    public static int bfs(int r, int c) {
+    public static void bfs() {
         Queue<Point> queue = new LinkedList<>();
-        boolean[][] visit = new boolean[N][M];
-        int dist = 0;
-
-        queue.add(new Point(r, c));
-        visit[r][c] = true;
+        queue.addAll(sharks);
 
         while(!queue.isEmpty()) {
-            int len = queue.size();
+            Point now = queue.poll();
 
-            for (int i = 0; i < len; i++) {
-                Point now = queue.poll();
-                if (arr[now.x][now.y] == 1) return dist;
+            for (int d = 0; d < 8; d++) {
+                int nr = now.x + delta[d][0];
+                int nc = now.y + delta[d][1];
 
-                for (int d = 0; d < 8; d++) {
-                    int nr = now.x + delta[d][0];
-                    int nc = now.y + delta[d][1];
-
-                    if (0 <= nr && nr < N && 0 <= nc && nc < M && !visit[nr][nc]) {
-                        queue.add(new Point(nr, nc));
-                        visit[nr][nc] = true;
-                    }
+                if (0 <= nr && nr < N && 0 <= nc && nc < M
+                        && (arr[nr][nc] == 0 || arr[nr][nc] > arr[now.x][now.y] + 1)) {
+                    queue.add(new Point(nr, nc));
+                    arr[nr][nc] = arr[now.x][now.y] + 1;
+                    maxDist = Math.max(maxDist, arr[nr][nc] - 1);
                 }
             }
-            dist++;
         }
-        return dist;
     }
 }
