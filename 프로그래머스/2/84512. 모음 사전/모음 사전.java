463efe795546b1173dfdd8e1010ab25d416a8329
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2024-10-19 00:37:12 +0900

    [level 2] Title: 모음 사전, Time: 27.66 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public List<String> list = new ArrayList<>();
-    public boolean isFind = false;
-    public int cnt = 0;
-    
-    public void dfs(String word, String str, int len) {
-        if (isFind) return;
-        if (len > 5) return;
-        
-        list.add(str);
-        if (str.equals(word)) isFind = true;
-        
-        for (int i = 0; i < 5; i ++) {
-            dfs(word, str + "AEIOU".charAt(i), len + 1);
-        }
-    }
+    ArrayList<String> list = new ArrayList<>();
     
     public int solution(String word) {
-        dfs(word, "", 0);
+        func(word, "", 0);
+        Collections.sort(list);
         return list.indexOf(word);
+    }
+    
+    public void func(String word, String cur, int len) {
+        if (len > 5) return;
+        list.add(cur);
+        for (int i = 0; i < 5; i++) {
+            func(word, cur + "AEIOU".charAt(i), len + 1);
+        }
     }
 }
