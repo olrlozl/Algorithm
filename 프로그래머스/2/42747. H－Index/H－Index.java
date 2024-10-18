c5b324ddfd8f49e7c0d091dd46b4865413735770
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2024-10-18 22:27:11 +0900

    [level 2] Title: H-Index, Time: 0.96 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,13 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
+        
         for (int h = citations.length; h > 0; h--) {
             if (h <= citations[citations.length - h]) {
                 return h;
             }
         }
+        
         return 0;
     }
 }
+
