0d1e3ff4affc5b11a188a81415a1d78cc23fa9ca
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2024-10-18 17:11:39 +0900

    [level 3] Title: 순위, Time: 16.50 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int n, int[][] results) {
+    public int solution(int n, int[][] results) {       
         int[][] arr = new int[n + 1][n + 1];
-        for (int[] result : results) {
-            arr[result[0]][result[1]] = 1;
-            arr[result[1]][result[0]] = -1;            
+        
+        for (int[] r: results) {
+            arr[r[0]][r[1]] = 1;
+            arr[r[1]][r[0]] = -1;
         }
         
         for (int i = 1; i <= n; i++) {
@@ -12,11 +15,10 @@ class Solution {
                     if (arr[i][k] == 1 && arr[k][j] == 1) {
                         arr[i][j] = 1;
                         arr[j][i] = -1;
-                    }
-                    if (arr[i][k] == -1 && arr[k][j] == -1) {
+                    } else if (arr[i][k] == -1 && arr[k][j] == -1) {
                         arr[i][j] = -1;
                         arr[j][i] = 1;
-                    }
+                    } 
                 }
             }
         }
@@ -30,6 +32,7 @@ class Solution {
             }
             if (cnt == 1) answer++;
         }
+        
         return answer;
     }
 }
