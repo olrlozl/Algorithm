2b9ba98f3a1f108de52717f7b524ae282c600769
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-01-04 00:07:56 +0900

    [Gold III] Title: 치즈, Time: 244 ms, Memory: 41424 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -3,33 +3,11 @@ import java.util.*;
 import java.awt.*;
 
 public class Main {
-    public static int n; // 세로
-    public static int m; // 가로
-    public static int[][] board;
-    public static int[][] delta = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
-    public static void bfs (Point p) {
-        Queue<Point> queue = new LinkedList<>();
-        boolean[][] visit = new boolean[n][m];
-
-        queue.add(new Point(p.x, p.y));
-        visit[p.x][p.y] = true;
-        board[p.x][p.y] = 2;
-
-        while(!queue.isEmpty()) {
-            Point cur = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nr = cur.x + delta[i][0];
-                int nc = cur.y + delta[i][1];
-                if (0 <= nr && nr < n && 0 <= nc && nc < m
-                        && !visit[nr][nc] && (board[nr][nc] == 0 || board[nr][nc] == 2)) {
-                    board[nr][nc] = 2;
-                    queue.add(new Point(nr, nc));
-                    visit[nr][nc] = true;
-                }
-            }
-        }
-    }
+    public static int N;
+    public static int M;
+    public static int[][] arr;
+    public static int[][] delta = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // 동 서 남 북
+    public static int cheese;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,51 +15,73 @@ public class Main {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); // 세로
-        m = Integer.parseInt(st.nextToken()); // 가로
-        board = new int[n][m];
-        int cheese = 0; // 치즈 개수
-        int time = 0; // 치즈가 모두 녹는데 걸리는 시간
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
+        arr = new int[N][M];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if (board[i][j] == 1) {
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
                     cheese++;
                 }
             }
         }
 
+        int time = 0;
+
         while (cheese > 0) {
-            bfs(new Point(0,0)); // 외부공기 2로 변경
-
-            for (int r = 0; r < n; r++) {
-                for (int c = 0; c < m; c++) {
-
-                    if (board[r][c] == 1) {
-                        int outAir = 0;
-
-                        for (int d = 0; d < 4; d++) {
-                            int nr = r + delta[d][0];
-                            int nc = c + delta[d][1];
-                            if (0 <= nr && nr < n && 0 <= nc && nc < m
-                                    && board[nr][nc] == 2) {
-                                outAir++;
-                            }
-                        }
-
-                        if (outAir >= 2) {
-                            board[r][c] = 0;
-                            cheese--;
-                        }
+            bfs();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (arr[i][j] == 1) {
+                        tryMelt(i, j);
                     }
                 }
             }
             time++;
         }
 
-        bw.write(time + "\n");
+        bw.write(time + "");
         bw.close();
+    }
+
+    public static void bfs() {
+        Queue<Point> queue = new LinkedList<>();
+        boolean[][] visit = new boolean[N][M];
+
+        queue.add(new Point(0, 0));
+        arr[0][0] = 2;
+        visit[0][0] = true;
+
+        while(!queue.isEmpty()) {
+            Point now = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nr = now.x + delta[i][0];
+                int nc = now.y + delta[i][1];
+                if (0 <= nr && nr < N && 0 <= nc && nc < M && !visit[nr][nc] && (arr[nr][nc] == 0 || arr[nr][nc] == 2)) {
+                    queue.add(new Point(nr, nc));
+                    visit[nr][nc] = true;
+                    arr[nr][nc] = 2;
+                }
+            }
+        }
+    }
+
+    public static void tryMelt(int r, int c) {
+        int outline = 0;
+        for (int i = 0; i < 4; i++) {
+            int nr = r + delta[i][0];
+            int nc = c + delta[i][1];
+            if (arr[nr][nc] == 2) {
+                outline++;
+                if (outline >= 2) {
+                    arr[r][c] = 0;
+                    cheese--;
+                    return;
+                }
+            }
+        }
     }
 }
