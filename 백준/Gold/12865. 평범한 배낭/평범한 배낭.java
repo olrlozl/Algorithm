16202d5ede5160f2fee9f013aabbfe6d16c7ec0e
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-01-11 00:24:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 14756 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -11,27 +11,24 @@ public class Main {
         int n = Integer.parseInt(st.nextToken()); // 물건 개수
         int k = Integer.parseInt(st.nextToken()); // 가방에 담을 수 있는 최대 무게
 
-        int[] weight = new int[n + 1]; // 물건 무게
-        int[] cost = new int[n + 1]; // 물건 가치
+        int[] weight = new int[n]; // 물건 무게
+        int[] cost = new int[n]; // 물건 가치
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            weight[i] = Integer.parseInt(st.nextToken()); // 물건 부피
+            weight[i] = Integer.parseInt(st.nextToken()); // 물건 무게
             cost[i] = Integer.parseInt(st.nextToken()); // 물건 가치
         }
 
-        int[][] arr = new int[n + 1][k + 1];
+        int[] dp = new int[k + 1];
 
-        for (int i = 1; i <= n; i++) {
-            for (int w = 1; w <= k; w++) {
-                if (weight[i] > w) {
-                    arr[i][w] = arr[i-1][w];
-                } else {
-                    arr[i][w] = Math.max(arr[i-1][w], arr[i-1][w-weight[i]] + cost[i]);
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = k; j >= weight[i]; j--) { // 뒤에서부터 순회해야함 (앞에서부터 갱신하면, 이미 갱신된 값이 뒤 계산에 영항 미침)
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + cost[i]);
             }
         }
-        bw.write(arr[n][k] + "");
+
+        bw.write(dp[k] + "");
         bw.close();
     }
 }
