5b636b441cfa2b74f7e335f81d19fb5be521b914
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-02-06 15:47:30 +0900

    [Gold V] Title: 하노이 탑, Time: 376 ms, Memory: 50224 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.java
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.java
@@ -6,13 +6,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 원판 개수
+        int N = Integer.parseInt(br.readLine());
         bw.write(BigInteger.valueOf(2).pow(N).subtract(new BigInteger("1")) + "\n");
         if (N <= 20) hanoi(N, 1, 2, 3);
         bw.close();
     }
 
-    public static void hanoi(int N, int from, int mid, int to) throws IOException {
+    public static void hanoi(int N, int from, int mid, int to) throws IOException{
         if (N == 1) {
             bw.write(from + " " + to + "\n");
             return;
