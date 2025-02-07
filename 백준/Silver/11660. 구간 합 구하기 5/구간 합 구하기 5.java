7e51db4f91ae3eb7204263cb30dfe9de58be78e2
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-02-07 19:54:42 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 816 ms, Memory: 121348 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,10 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
@@ -13,6 +12,7 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         int[][] dp = new int[N + 1][N + 1];
+
         for (int r = 1; r <= N; r++) {
             st = new StringTokenizer(br.readLine());
             for (int c = 1; c <= N; c++) {
@@ -20,13 +20,13 @@ public class Main {
             }
         }
 
-        for (int i = 1; i <= M; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int r1 = Integer.parseInt(st.nextToken());
             int c1 = Integer.parseInt(st.nextToken());
             int r2 = Integer.parseInt(st.nextToken());
             int c2 = Integer.parseInt(st.nextToken());
-            bw.write((dp[r2][c2] - dp[r1 - 1][c2] - dp[r2][c1 - 1] + dp[r1 - 1][c1 - 1]) + "\n");
+            bw.write(dp[r2][c2] - dp[r1 - 1][c2] - dp[r2][c1 - 1] + dp[r1 - 1][c1 - 1] + "\n");
         }
         bw.close();
     }
