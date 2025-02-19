b2441f508018a5fce773ff1301178997285ab596
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-02-19 19:04:36 +0900

    [level 3] Title: 이중우선순위큐, Time: 90.49 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> min = new PriorityQueue<>();
-        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+        TreeSet<Integer> set = new TreeSet<>();
         
         for (String operation: operations) {
             char order = operation.charAt(0);
             int num = Integer.parseInt(operation.split(" ")[1]);
             if (order == 'I') {
-                min.add(num);
-                max.add(num);
+                set.add(num);
             } else {
-                if (min.isEmpty()) continue;
+                if (set.isEmpty()) continue;
                 if (num == 1) {
-                    min.remove(max.poll());
+                    set.pollLast();
                 } else {
-                    max.remove(min.poll());
+                    set.pollFirst();
                 }
             }
         }
         
-        if (min.isEmpty()) return new int[] {0, 0};
-        else return new int[] {max.poll(), min.poll()};
+        if (set.isEmpty()) return new int[] {0, 0};
+        else return new int[] {set.last(), set.first()};
     }
 }
