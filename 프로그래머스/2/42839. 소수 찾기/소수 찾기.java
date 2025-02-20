34b1b43334b195e47ae397246c9702ee98903417
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-02-20 15:44:57 +0900

    [level 2] Title: 소수 찾기, Time: 4.58 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -7,39 +7,37 @@ class Solution {
         char[] src = numbers.toCharArray();
         
         for (int i = 1; i <= src.length; i++) {
-            boolean[] visit = new boolean[src.length];
             char[] tgt = new char[i];
-            perm(src, visit, tgt, 0);
+            boolean[] visit = new boolean[src.length];
+            perm(src, tgt, visit, 0);
         }
         
         return set.size();
     }
     
-    // 중복 순열
-    public void perm(char[] src, boolean[] visit, char[] tgt, int tgtIdx) {
+    public void perm(char[] src, char[] tgt, boolean[] visit, int tgtIdx) {
         if (tgtIdx == tgt.length) {
-            int num = 0;
-            for (char c : tgt) num += (c - '0') * Math.pow(10, --tgtIdx);
+            StringBuilder sb = new StringBuilder();
+            for (char c : tgt) sb.append(c);
+            int num = Integer.parseInt(sb.toString());
             if (isPrime(num)) set.add(num);
             return;
         }
-        
         boolean[] select = new boolean[10];
         for (int i = 0; i < src.length; i++) {
             if (!visit[i] && !select[src[i] - '0']) {
                 tgt[tgtIdx] = src[i];
-                select[src[i] - '0'] = true;
                 visit[i] = true;
-                perm(src, visit, tgt, tgtIdx + 1);
+                select[src[i] - '0'] = true;
+                perm(src, tgt, visit, tgtIdx + 1);
                 visit[i] = false;
             }
         }
     }
     
-    // 소수 판별
     public boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i <= (int)Math.sqrt(n); i++) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
