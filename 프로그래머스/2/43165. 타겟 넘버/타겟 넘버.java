1d1df643f41a62ccbcc328f4b120ed26901b56be
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-02-20 11:13:21 +0900

    [level 2] Title: 타겟 넘버, Time: 7.83 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,17 @@
 class Solution {
-    public int cnt = 0;
+    public int cnt;
     
     public int solution(int[] numbers, int target) {
-        recursive(numbers, target, 0, 0);
+        func(numbers, target, 0, 0);
         return cnt;
     }
     
-    public void recursive(int[] numbers, int target, int sum, int idx) {
-        if (numbers.length == idx) {
-            if (sum == target) {
-                cnt++;
-            }
+    public void func(int[] numbers, int target, int res, int idx) {
+        if (idx == numbers.length) {
+            if (res == target) cnt++;
             return;
         }
-        recursive(numbers, target, sum + numbers[idx], idx + 1);
-        recursive(numbers, target, sum - numbers[idx], idx + 1);
+        func(numbers, target, res + numbers[idx], idx + 1);
+        func(numbers, target, res - numbers[idx], idx + 1);
     }
 }
