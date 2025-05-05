73703ac7577cd63825725daa69dc733a71739bb0
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-05-05 19:09:56 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 608 ms, Memory: 121264 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static List<Integer> list;
+
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,10 +17,10 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<Integer> list = new ArrayList<>();
+        list = new ArrayList<>();
 
         for (int num : arr) {
-            int idx = Collections.binarySearch(list, num);
+            int idx = binarySearch(num);
             if (idx < 0) idx = - idx- 1;
 
             if (idx == list.size()) list.add(num);
@@ -27,5 +29,17 @@ public class Main {
 
         bw.write(list.size() + "");
         bw.close();
+    }
+
+    public static int binarySearch(int num) {
+        int left = 0;
+        int right = list.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (num <= list.get(mid)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
     }
 }
