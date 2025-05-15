1c641bddf2e8a7180cfe3166ec5ed5c5a51a78bb
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-05-15 16:32:27 +0900

    [level 3] Title: 네트워크, Time: 1.27 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,47 +1,50 @@
 import java.util.*;
 
 class Solution {
-    public void bfs(ArrayList<Integer>[] graph, boolean[] visit, int x) {
+    public boolean[] visit;
+    public ArrayList<Integer>[] graph;
+    
+    public int solution(int n, int[][] computers) {
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (computers[i][j] == 1) {
+                    graph[i].add(j);
+                    graph[j].add(i);
+                }
+            }
+        }
+        
+        visit = new boolean[n];
+        int cnt = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                bfs(i);
+                cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+    
+    public void bfs(int start) {
         Queue<Integer> queue = new LinkedList<>();
         
-        queue.add(x);
-        visit[x] = true;
+        queue.add(start);
+        visit[start] = true;
         
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            for (int next : graph[cur]) {
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+            for (int next: graph[now]) {
                 if (!visit[next]) {
                     queue.add(next);
                     visit[next] = true;
                 }
             }
         }
-    }
-    
-    public int solution(int n, int[][] computers) {
-        int cnt = 0;
-        
-        ArrayList<Integer>[] graph = new ArrayList[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && computers[i][j] == 1) {
-                    graph[i + 1].add(j + 1);
-                }
-            }
-        }
-        
-        boolean[] visit = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if (!visit[i]) {
-                bfs(graph, visit, i);
-                cnt++;
-            }
-        }
-        
-        return cnt;
     }
 }
