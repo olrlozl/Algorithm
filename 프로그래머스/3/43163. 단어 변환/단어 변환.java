26c10cb96e2932cf0171a006740e43ad23570b6c
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-05-15 18:03:28 +0900

    [level 3] Title: 단어 변환, Time: 0.18 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -5,32 +5,23 @@ class Solution {
         Queue<String> queue = new LinkedList<>();
         boolean[] visit = new boolean[words.length];
         
-        int cnt = -1;
+        int cnt = 0;
         queue.add(begin);
         
         while (!queue.isEmpty()) {
-            cnt++;
             int size = queue.size();
             
             for (int i = 0; i < size; i++) {
                 String now = queue.poll();
-                
-                if (now.equals(target)) {
-                    return cnt;
-                }
+                if (now.equals(target)) return cnt;
 
                 for (int j = 0; j < words.length; j++) {
                     if (visit[j]) continue;
-                    
                     int diff = 0;
-                    
+
                     for (int k = 0; k < begin.length(); k++) {
-                        if (now.charAt(k) != words[j].charAt(k)) {
-                            diff++;
-                        }
-                        if (diff > 1) {
-                            break;
-                        }
+                        if (now.charAt(k) != words[j].charAt(k)) diff++;
+                        if (diff > 1) break;
                     }
 
                     if (diff == 1) {
@@ -39,6 +30,7 @@ class Solution {
                     }
                 }
             }
+            cnt++;
         }
         return 0;
     }
