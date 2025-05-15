9a59b9da802d59463e271d4ac80479702ab8992f
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-05-15 18:34:15 +0900

    [level 1] Title: K번째수, Time: 4.10 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,7 +4,6 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int idx = 0;
-        
         for (int[] c: commands) {
             int[] arr = new int[c[1] - c[0] + 1];
             for (int i = c[0] - 1, j = 0; i < c[1]; i++) {
@@ -13,7 +12,6 @@ class Solution {
             Arrays.sort(arr);
             answer[idx++] = arr[c[2] - 1];
         }
-        
         return answer;
     }
 }
