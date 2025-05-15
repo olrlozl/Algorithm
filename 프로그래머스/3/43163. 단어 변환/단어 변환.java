3fbd20347bf84d848d6454842ad1f3830d2a2487
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-05-15 18:00:29 +0900

    [level 3] Title: 단어 변환, Time: 0.20 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -4,32 +4,41 @@ class Solution {
     public int solution(String begin, String target, String[] words) {
         Queue<String> queue = new LinkedList<>();
         boolean[] visit = new boolean[words.length];
-        queue.add(begin);
-        int cnt = 0;
         
-        loop: while(!queue.isEmpty()) {
-            int len = queue.size();
-            for (int i = 0; i < len; i++) {
-                String word = queue.poll();
-                if (word.equals(target)) {
+        int cnt = -1;
+        queue.add(begin);
+        
+        while (!queue.isEmpty()) {
+            cnt++;
+            int size = queue.size();
+            
+            for (int i = 0; i < size; i++) {
+                String now = queue.poll();
+                
+                if (now.equals(target)) {
                     return cnt;
                 }
+
                 for (int j = 0; j < words.length; j++) {
                     if (visit[j]) continue;
-                    int dif = 0;
+                    
+                    int diff = 0;
+                    
                     for (int k = 0; k < begin.length(); k++) {
-                        if (word.charAt(k) != words[j].charAt(k)) {
-                            dif++;
-                            if (dif > 1) break;
+                        if (now.charAt(k) != words[j].charAt(k)) {
+                            diff++;
+                        }
+                        if (diff > 1) {
+                            break;
                         }
                     }
-                    if (dif == 1) {
+
+                    if (diff == 1) {
                         queue.add(words[j]);
                         visit[j] = true;
                     }
                 }
             }
-            cnt++;
         }
         return 0;
     }
