8c19f244a1eb8585e83da1c132c6a0d0bdd0c888
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-05-15 21:17:30 +0900

    [level 2] Title: 소수 찾기, Time: 5.50 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,29 +1,35 @@
 import java.util.*;
 
 class Solution {
+    public int cnt = 0;
     public HashSet<Integer> set = new HashSet<>();
     
     public int solution(String numbers) {
         char[] src = numbers.toCharArray();
         
-        for (int i = 1; i <= src.length; i++) {
+        for (int i = 1; i <= numbers.length(); i++) {
             char[] tgt = new char[i];
-            boolean[] visit = new boolean[src.length];
+            boolean[] visit = new boolean[numbers.length()];
             perm(src, tgt, visit, 0);
         }
         
-        return set.size();
+        for (int n : set) {
+            if (isPrime(n)) cnt++;
+        }
+            
+        return cnt;
     }
     
     public void perm(char[] src, char[] tgt, boolean[] visit, int tgtIdx) {
         if (tgtIdx == tgt.length) {
             StringBuilder sb = new StringBuilder();
             for (char c : tgt) sb.append(c);
-            int num = Integer.parseInt(sb.toString());
-            if (isPrime(num)) set.add(num);
+            set.add(Integer.parseInt(sb.toString()));
             return;
         }
+        
         boolean[] select = new boolean[10];
+        
         for (int i = 0; i < src.length; i++) {
             if (!visit[i] && !select[src[i] - '0']) {
                 tgt[tgtIdx] = src[i];
@@ -36,11 +42,9 @@ class Solution {
     }
     
     public boolean isPrime(int n) {
-        if (n <= 1) return false;
+        if (n < 2) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
+            if (n % i == 0) return false;
         }
         return true;
     }
