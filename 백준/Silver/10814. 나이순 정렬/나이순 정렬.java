8b38fa4a2a802c486232fb8dffacdd66ad126164
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-06-03 00:05:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 728 ms, Memory: 56988 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,25 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
-        String[][] arr = new String[n][2];
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             arr[i][0] = st.nextToken();
             arr[i][1] = st.nextToken();
         }
 
-        Arrays.sort(arr, ((o1, o2) -> Integer.parseInt(o1[0]) - Integer.parseInt(o2[0])));
+        Arrays.sort(arr, (a, b) -> Integer.parseInt(a[0]) - Integer.parseInt(b[0]));
 
-        for (int i = 0; i < n; i++) bw.write(arr[i][0] + " " + arr[i][1] + "\n");
-
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
         bw.close();
     }
 }
