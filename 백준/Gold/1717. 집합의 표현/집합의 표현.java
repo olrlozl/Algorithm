ca7079540dac3018e3cc1290298a34fa96896684
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-06-03 19:33:25 +0900

    [Gold V] Title: 집합의 표현, Time: 424 ms, Memory: 52840 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,45 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int n, m;
+    public static int[] parent;
 
-    public static void union(int[] parent, int a, int b) {
-        a = find(parent, a);
-        b = find(parent, b);
-        if (a < b) parent[b] = a;
-        else parent[a] = b;
-    }
-
-    public static int find (int[] parent, int x) {
-        if (parent[x] == x) return x;
-        else return parent[x] = find(parent, parent[x]); // 부모노드 대신 루트노드로 재설정
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 초기 집합: {0}, {1}, {2}, ... , {n}
-        int m = Integer.parseInt(st.nextToken()); // 연산 개수
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        // 부모노드 초기화
-        int[] parent = new int[n + 1];
-        for (int i = 0; i < n + 1; i++) parent[i] = i;
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) parent[i] = i;
 
-        // m개의 연산
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int od = Integer.parseInt(st.nextToken()); // 연산 종류
-            int a = Integer.parseInt(st.nextToken()); // a 집합
-            int b = Integer.parseInt(st.nextToken()); // b 집합
+            int order = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            if (od == 0) { // 합집합
-                union(parent, a, b);
-            } else if (od == 1) { // 같은 집합에 포함 되어 있는지 확인
-                bw.write(find(parent, a) == find(parent, b) ? "YES\n" : "NO\n");
+            if (order == 0) {
+                union(a, b);
+            } else {
+                if (find(a) == find(b)) bw.write("YES\n");
+                else bw.write("NO\n");
             }
         }
+
         bw.close();
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x < y) parent[x] = y;
+        else if (x > y) parent[y] = x;
+    }
+
+    public static int find(int x) {
+        if (parent[x] == x) return x;
+        return find(parent[x]);
     }
 }
