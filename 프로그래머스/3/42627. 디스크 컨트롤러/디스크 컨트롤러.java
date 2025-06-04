0c09172696a493e06bb8e12d7ea2fdfd638776b4
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-06-04 19:13:41 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.52 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,43 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        int[] endTime = new int[jobs.length];
+        int answer = 0;
         
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]); // 요청시각이 빠른 순 정렬
         
         PriorityQueue<Work> pq = new PriorityQueue<>();
         int curTime = 0;
-        int curIdx = 0;
+        int jobIdx = 0;
         int finish = 0;
         
         while (finish < jobs.length) {
-            while (curIdx < jobs.length && jobs[curIdx][0] <= curTime) {
-                pq.add(new Work(curIdx, jobs[curIdx][0], jobs[curIdx][1]));
-                curIdx++;
+            while (jobIdx < jobs.length && jobs[jobIdx][0] <= curTime) {
+                pq.add(new Work(jobs[jobIdx][0], jobs[jobIdx][1]));
+                jobIdx++;
             }
-            if (pq.isEmpty() && curIdx < jobs.length) {
-                curTime++;
+            if (pq.isEmpty() && jobIdx < jobs.length) {
+                curTime = jobs[jobIdx][0];
             } else {
                 Work w = pq.poll();
                 curTime += w.require;
-                endTime[w.idx] = curTime - w.in;
+                answer += curTime - w.in;
                 finish++;
             }
         }
         
-        int avg = 0;
-        for (int t : endTime)  avg += t;
-        avg /= jobs.length;
-        return avg;
+        return answer / jobs.length;
     }
     
     public class Work implements Comparable<Work>{
-        int idx; // 작업번호
         int in; // 요청시각
         int require; // 소요시간
         
-        public Work (int idx, int in, int require) {
-            this.idx = idx;
+        public Work (int in, int require) {
             this.in = in;
             this.require = require;
         }
@@ -46,8 +41,7 @@ class Solution {
         @Override
         public int compareTo(Work o) {
             if (this.require != o.require) return this.require - o.require;
-            if (this.in != o.in) return this.in - o.in;
-            return this.idx - o.idx;
+            return this.in - o.in;
         }
     }
 }
