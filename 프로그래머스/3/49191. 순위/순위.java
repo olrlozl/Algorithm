2d6a1071ae4d6ff8706d2f6cf59e9d875a7e56b3
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-06-04 17:50:08 +0900

    [level 3] Title: 순위, Time: 13.33 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,33 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int[][] results) {       
-        int[][] arr = new int[n + 1][n + 1];
+    public int solution(int n, int[][] results) {
+        int answer = 0;
         
-        for (int[] r: results) {
-            arr[r[0]][r[1]] = 1;
-            arr[r[1]][r[0]] = -1;
+        int[][] graph = new int[n + 1][n  + 1];
+        
+        for (int[] r : results) {
+            graph[r[0]][r[1]] = 1;
+            graph[r[1]][r[0]] = -1;
         }
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    if (arr[i][k] == 1 && arr[k][j] == 1) {
-                        arr[i][j] = 1;
-                        arr[j][i] = -1;
-                    } else if (arr[i][k] == -1 && arr[k][j] == -1) {
-                        arr[i][j] = -1;
-                        arr[j][i] = 1;
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    } else if (graph[i][k] == -1 && graph[k][j] == -1) {
+                        graph[i][j] = -1;
                     } 
                 }
             }
         }
         
-        int answer = 0;
+        
         for (int i = 1; i <= n; i++) {
             int cnt = 0;
             for (int j = 1; j <= n; j++) {
-                if (arr[i][j] == 0) cnt++;
+                if (graph[i][j] == 0) cnt++;
                 if (cnt > 1) break;
             }
             if (cnt == 1) answer++;
