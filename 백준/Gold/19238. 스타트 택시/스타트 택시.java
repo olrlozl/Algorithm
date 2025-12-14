d10cc5380e186d9f9646e40bfea286061b1ba9f2
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2025-12-14 20:25:18 +0900

    [Gold II] Title: 스타트 택시, Time: 188 ms, Memory: 22340 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.java
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.java
@@ -3,13 +3,14 @@ import java.util.*;
 import java.awt.*;
 
 public class Main {
-    static int N, M, fare;
+    static int N, M;
+    static long fuel;
     static int[][] arr;
     static Point texi = new Point();
-    static int[][] passenger;
-    static boolean[] arrive;
+    static int[][] passenger; // 출발지, 도착지
+    static int[][] pidMap; // 승객 id, 없으면 -1
+    static boolean[] arrive; // 승객 도착 여부
     static int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static int[][] dist; // 택시로부터의 거리
     static int nearestP; // 가장 가까운 손님
     static int minDist; // 가장 가까운 손님과의 거리
 
@@ -20,12 +21,13 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        fare = Integer.parseInt(st.nextToken());
+        fuel = Integer.parseInt(st.nextToken());
 
         arr = new int[N][N];
         passenger = new int[M][4];
         arrive = new boolean[M];
-        dist = new int[N][N];
+        pidMap = new int[N][N];
+        for (int i = 0; i < N; i++) Arrays.fill(pidMap[i], -1);
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -40,41 +42,56 @@ public class Main {
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            passenger[i][0] = Integer.parseInt(st.nextToken()) - 1;
-            passenger[i][1] = Integer.parseInt(st.nextToken()) - 1;
+            int sr = Integer.parseInt(st.nextToken()) - 1;
+            int sc = Integer.parseInt(st.nextToken()) - 1;
             passenger[i][2] = Integer.parseInt(st.nextToken()) - 1;
             passenger[i][3] = Integer.parseInt(st.nextToken()) - 1;
+
+            pidMap[sr][sc] = i;
+            passenger[i][0] = sr;
+            passenger[i][1] = sc;
         }
 
         for (int i = 0; i < M; i++) {
-            calDist();
-            goToNearestPassenger();
+            findPassenger();
+            goToPassenger();
             goToDest();
         }
 
-        boolean flag = true;
-        for (int i = 0; i < M; i++) {
-            if (!arrive[i]) {
-                flag = false;
-                break;
-            }
-        }
-        if (flag) System.out.println(fare);
-        else System.out.println(-1);
+        System.out.println(fuel);
     }
 
-    public static void calDist() {
+    public static void findPassenger() {
+        nearestP = -1;
+        minDist = Integer.MAX_VALUE;
+        int bestR = Integer.MAX_VALUE;
+        int bestC = Integer.MAX_VALUE;
+
         Queue<Point> queue = new LinkedList<>();
-        dist = new int[N][N];
+        int[][] dist = new int[N][N];
 
         queue.add(new Point(texi.x, texi.y));
         dist[texi.x][texi.y] = 1;
 
         while(!queue.isEmpty()) {
             Point p = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nr = p.x + delta[i][0];
-                int nc = p.y + delta[i][1];
+            int pid = pidMap[p.x][p.y];
+            int d = dist[p.x][p.y] - 1;
+
+            if (d > minDist) break; // 더 먼 거리의 승객은 볼 필요 없음
+
+            if (pid != -1 && !arrive[pid]) {
+                if (minDist == Integer.MAX_VALUE) minDist = d;
+                if (p.x < bestR || (p.x == bestR && p.y < bestC)) {
+                    bestR = p.x;
+                    bestC = p.y;
+                    nearestP = pid;
+                }
+            }
+
+            for (int j = 0; j < 4; j++) {
+                int nr = p.x + delta[j][0];
+                int nc = p.y + delta[j][1];
                 if (0 <= nr && nr < N && 0 <= nc && nc < N && arr[nr][nc] == 0 && dist[nr][nc] == 0) {
                     queue.add(new Point(nr, nc));
                     dist[nr][nc] = dist[p.x][p.y] + 1;
@@ -83,36 +100,19 @@ public class Main {
         }
     }
 
-    public static void goToNearestPassenger() {
-        minDist = Integer.MAX_VALUE;
-
-        for (int i = 0; i < M; i++) {
-            if (arrive[i]) continue;
-
-            int r = passenger[i][0];
-            int c = passenger[i][1];
-
-            if (dist[r][c] == 0) continue;
-
-            if (minDist > dist[r][c] - 1) {
-                minDist = dist[r][c] - 1;
-                nearestP = i;
-            } else if (minDist == dist[r][c] - 1) {
-                if (r < passenger[nearestP][0] || (r == passenger[nearestP][0] && c < passenger[nearestP][1])) {
-                    nearestP = i;
-                }
-            }
-        }
-
+    public static void goToPassenger() {
         if (minDist == Integer.MAX_VALUE) {
             System.out.println(-1);
             System.exit(0);
         }
 
-        if (fare >= minDist) {
-            fare -= minDist;
-            texi.x = passenger[nearestP][0];
-            texi.y = passenger[nearestP][1];
+        if (fuel >= minDist) {
+            fuel -= minDist;
+            int sr = passenger[nearestP][0];
+            int sc = passenger[nearestP][1];
+            texi.x = sr;
+            texi.y = sc;
+            pidMap[sr][sc] = -1;
         } else {
             System.out.println(-1);
             System.exit(0);
@@ -135,8 +135,8 @@ public class Main {
         while(!queue.isEmpty()) {
             Point p = queue.poll();
             if (p.x == er && p.y == ec) {
-                if (fare >= dist[er][ec] - 1) {
-                    fare += dist[er][ec] - 1;
+                if (fuel >= dist[er][ec] - 1) {
+                    fuel += dist[er][ec] - 1;
                     texi.x = er;
                     texi.y = ec;
                     arrive[nearestP] = true;
