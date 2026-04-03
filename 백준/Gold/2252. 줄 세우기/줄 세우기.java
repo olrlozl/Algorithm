d504c51bd9c379cfd888b81b8e1cdbea3d24d07c
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2026-04-03 16:19:12 +0900

    [Gold III] Title: 줄 세우기, Time: 748 ms, Memory: 52348 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,24 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 학생 수
-        int m = Integer.parseInt(st.nextToken()); // 키 비교 횟수
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        // 각 노드로의 진입 차수
-        int[] edgeCnt = new int[n + 1];
+        int[] edgeCnt = new int[N + 1];
 
-        // 각 노드가 집입하는 노드들의 정보
-        ArrayList<Integer>[] graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+        ArrayList<Integer>[] graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) graph[i] = new ArrayList<>();
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
@@ -27,23 +23,20 @@ public class Main {
             edgeCnt[b]++;
         }
 
-        Queue<Integer> queue = new LinkedList<>(); // 큐
+        Queue<Integer> queue = new LinkedList<>();
 
-        // 진입 차수의 값이 0인 노드가 있다면 큐에 넣기
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i <= N; i++) {
             if (edgeCnt[i] == 0) queue.add(i);
+        }
 
-        while (!queue.isEmpty()) { // 큐가 빌 때 까지
-            int now = queue.poll(); // 큐에서 노드 하나를 꺼내고
-            bw.write(now + " ");
-            for (int next : graph[now]) { // 진입 가능한 다음 노드가 있다면
-                edgeCnt[next]--; // 진입 차수 감소 후
-                if (edgeCnt[next] == 0) { // 진입 차수의 값이 0이 되었다면
-                    queue.add(next); // 큐에 넣기
+        while(!queue.isEmpty()) {
+            int x = queue.poll();
+            System.out.print(x + " ");
+            for (int nx : graph[x]) {
+                if (--edgeCnt[nx] == 0) {
+                    queue.add(nx);
                 }
             }
         }
-
-        bw.close();
     }
 }
