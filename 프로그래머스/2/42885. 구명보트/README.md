1393732a7f6111e9f32f09fb9258f8aa4b04715a
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2026-05-29 16:16:55 +0900

    [level 2] Title: 구명보트, Time: 5.82 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 60.4 MB, 시간: 8.95 ms
+메모리: 59 MB, 시간: 5.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 15:49:39
+2026년 05월 29일 16:16:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
+        // 가장 무거운 사람은 무조건 보드를 한 번 타야함.
+        // 만약 가장 가벼운 사람과 같이 탈 수 있으면 같이 태우고, 못타면 무거운 사람 혼자 태움.
         Arrays.sort(people);
-
-        int boat = 0;
+        int answer = 0;
         int l = 0;
         int r = people.length - 1;
-
+        
         while (l <= r) {
-            if (people[l] + people[r] <= limit) l++;
-            boat++;
+            if (people[l] + people[r] <= limit) {
+                l++;
+            } 
+            answer++;
             r--;
         }
-
-        return boat;
+        return answer;
     }
 }
