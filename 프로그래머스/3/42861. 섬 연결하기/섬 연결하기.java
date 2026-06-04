f319552a7e774b1171464f5063330875b9c6de11
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2026-06-04 15:01:46 +0900

    [level 3] Title: 섬 연결하기, Time: 1.81 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -4,34 +4,51 @@ class Solution {
     int answer = 0;
     
     public int solution(int n, int[][] costs) {
-        int[] parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
+        ArrayList<Edge>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
         
-        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
-        kruskal(costs, parent);
-
+        for (int[] c : costs) {
+            graph[c[0]].add(new Edge(c[1], c[2]));
+            graph[c[1]].add(new Edge(c[0], c[2]));
+        }
+        
+        prim(graph, 0, n);
         return answer;
     }
     
-    public int find(int[] parent, int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent, parent[x]);
-    }
-    
-    public void union(int[] parent, int x, int y) {
-        x = find(parent, x);
-        y = find(parent, y);
+    public class Edge implements Comparable<Edge> {
+        int v;
+        int cost;
         
-        if (x < y) parent[y] = x;
-        else parent[x] = y;
+        public Edge(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+        
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
     }
     
-    public void kruskal(int[][] costs, int[] parent) {
-        for (int[] c : costs) {
-            if (find(parent, c[0]) != find(parent, c[1])) {
-                union(parent, c[0], c[1]);
-                answer += c[2];
-            } 
+    public void prim(ArrayList<Edge>[] graph, int start, int n) {
+        boolean[] visit = new boolean[n];
+        
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(start, 0));
+        
+        while (!pq.isEmpty()) {
+            Edge now = pq.poll();
+            
+            if (visit[now.v]) continue;
+            visit[now.v] = true;
+            answer += now.cost; //
+            
+            for (Edge next : graph[now.v]) {
+                if (!visit[next.v]) { //
+                    pq.add(next);
+                }
+            }
         }
     }
 }
