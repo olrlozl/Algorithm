9e9ebe05dd2d8ff1e24f0b5ac5dda325bad1fb0e
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2026-06-04 14:24:18 +0900

    [level 3] Title: 섬 연결하기, Time: 0.65 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,35 +1,37 @@
 import java.util.*;
 
 class Solution {
-    public int result = 0;
+    int answer = 0;
     
     public int solution(int n, int[][] costs) {
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        
         Arrays.sort(costs, (a, b) -> a[2] - b[2]);
-        Kruskal(n, costs);
-        return result;
+        kruskal(costs, parent);
+
+        return answer;
+    }
+    
+    public int find(int[] parent, int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent, parent[x]);
     }
     
     public void union(int[] parent, int x, int y) {
         x = find(parent, x);
         y = find(parent, y);
+        
         if (x < y) parent[y] = x;
         else parent[x] = y;
     }
     
-    public int find(int[] parent, int x) {
-        if (parent[x] == x) return x;
-        return find(parent, parent[x]);
-    }
-    
-    public void Kruskal(int n, int[][] costs) {
-        int[] parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-        
-        for (int[] cost: costs) {
-            if (find(parent, cost[0]) != find(parent, cost[1])) {
-                union(parent, cost[0], cost[1]);
-                result += cost[2];
-            }
+    public void kruskal(int[][] costs, int[] parent) {
+        for (int[] c : costs) {
+            if (find(parent, c[0]) != find(parent, c[1])) {
+                union(parent, c[0], c[1]);
+                answer += c[2];
+            } 
         }
     }
 }
