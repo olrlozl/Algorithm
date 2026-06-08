dde649b20dcb1ce5fc4df975bc801145538db640
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2026-06-08 16:48:13 +0900

    [level 3] Title: 베스트앨범, Time: 5.87 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -7,57 +7,33 @@ class Solution {
         for (int i = 0; i < genres.length; i++) {
             map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
         } 
-        // System.out.println(map);
         
         ArrayList<String> gList = new ArrayList<>(map.keySet()); // 장르 리스트 
         gList.sort((a, b) -> map.get(b) - map.get(a)); // 누적횟수 기준 내림차순
-        // System.out.println(gList);
         
-        ArrayList<Song>[] songs = new ArrayList[gList.size()]; // 각 장르별 {고유번호, 재생횟수} 리스트
-        for (int i = 0; i < songs.length; i++) songs[i] = new ArrayList<>();
+        HashMap<String, HashMap<Integer, Integer>> map2 = new HashMap<>();
         
         for (int i = 0; i < genres.length; i++) {
-            int gIdx = gList.indexOf(genres[i]); // 장르별 누적횟수로 내림차순했을 때 장르 인덱스
-            songs[gIdx].add(new Song(i, plays[i]));
-        }
-        
-        int albumSize = 0;
-        
-        for (int i = 0; i < songs.length; i++) {
-            Collections.sort(songs[i]); // 재생횟수 기준 내림차순 정렬
-            albumSize += Math.min(2, songs[i].size()); // 앨범 수록곡 카운팅
-            // System.out.println(gList.get(i) + " : " + songs[i]);
-        }
-        
-        int[] answer = new int[albumSize];
-        int aIdx = 0;
-        
-        for (int i = 0; i < songs.length; i++) {
-            int len = Math.min(2, songs[i].size()); // 현재 장르의 수록곡 개수
-            for (int j = 0; j < len; j++) {
-                answer[aIdx++] = songs[i].get(j).idx;
+            if (!map2.containsKey(genres[i])) {
+                HashMap<Integer, Integer> song = new HashMap<>();
+                song.put(i, plays[i]);
+                map2.put(genres[i], song);
+            } else {
+                map2.get(genres[i]).put(i, plays[i]);
             }
         }
-        return answer;
-    }
-    
-    public class Song implements Comparable<Song>{
-        int idx;
-        int play;
         
-        public Song(int idx, int play) {
-            this.idx = idx;
-            this.play = play;
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for (String g : gList) {
+            HashMap<Integer, Integer> song = map2.get(g);
+            ArrayList<Integer> songIdxList = new ArrayList<>(song.keySet());
+            songIdxList.sort((a, b) -> song.get(b) - song.get(a));
+            for (int i = 0; i < Math.min(2, song.size()); i++) {
+                answer.add(songIdxList.get(i));
+            }
         }
         
-        @Override
-        public int compareTo(Song o) {
-            return o.play - this.play;
-        }
-            
-        // @Override
-        // public String toString() {
-        //     return "{idx=" + idx + ", play=" + play + "}";
-        // }
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
