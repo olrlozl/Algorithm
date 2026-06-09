be844fbe67fb5dc76f49abe0437dafe324f14a77
LEEEUNZ <108621930+olrlozl@users.noreply.github.com>
2026-06-09 11:41:10 +0900

    [level 2] Title: 배달, Time: 2.31 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,41 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public int cnt = 0;
-    
     public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+
         ArrayList<Edge>[] graph = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
-        
+        for (int i = 0; i < N + 1; i++) graph[i] = new ArrayList<>();
         
         for (int[] r : road) {
             graph[r[0]].add(new Edge(r[1], r[2]));
             graph[r[1]].add(new Edge(r[0], r[2]));
         }
         
-        Dijkstra(graph, N, K);
+        int[] dist = Dijkstra(graph, N);
         
-        return cnt;
-    }
-    
-    public class Edge implements Comparable<Edge> {
-        int idx;
-        int cost;
-        
-        public Edge(int idx, int cost) {
-            this.idx = idx;
-            this.cost = cost;
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] <= K) answer++;
         }
         
-        @Override
-        public int compareTo(Edge o) {
-            return this.cost - o.cost;
-        }
+        return answer;
     }
     
-    public void Dijkstra(ArrayList<Edge>[] graph, int N, int K) {
-        boolean[] visit = new boolean[N + 1];
-        
+    public int[] Dijkstra(ArrayList<Edge>[] graph, int N) {
         PriorityQueue<Edge> pq = new PriorityQueue<>();
         pq.add(new Edge(1, 0));
         
@@ -43,24 +29,36 @@ class Solution {
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[1] = 0;
         
+        boolean[] visit = new boolean[N + 1];
         
         while(!pq.isEmpty()) {
-            int nowIdx = pq.poll().idx;
-            if (visit[nowIdx]) continue;
-            visit[nowIdx] = true;
+            Edge now = pq.poll();
             
-            for (Edge next: graph[nowIdx]) {
-                if (dist[next.idx] > dist[nowIdx] + next.cost) {
-                    dist[next.idx] = dist[nowIdx] + next.cost;
-                    pq.add(new Edge(next.idx, dist[next.idx]));
+            if (visit[now.v]) continue;
+            visit[now.v] = true;
+            
+            for (Edge next: graph[now.v]) {
+                if (dist[next.v] > dist[now.v] + next.cost) {
+                    dist[next.v] = dist[now.v] + next.cost;
+                    pq.add(new Edge(next.v, dist[next.v]));
                 }
             }
         }
+        return dist;
+    }
+    
+    public class Edge implements Comparable<Edge>{
+        int v;
+        int cost;
         
-        for (int i = 1; i <= N; i++) {
-            if (dist[i] <= K) {
-                cnt++;
-            }
+        public Edge(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+        
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
         }
     }
 }
